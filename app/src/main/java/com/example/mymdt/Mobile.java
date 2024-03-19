3bd9ea91980180082154482564d7c03040708423
kaverinegi10 <kaverinegi03@gmail.com>
2024-03-19 21:48:27 +0530

    my project

--- app/src/main/java/com/example/mymdt/Mobile.java
+++ app/src/main/java/com/example/mymdt/Mobile.java
@@ -1,14 +1,62 @@
-package com.example.mymdt;
+package com.example.scanwell;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class Mobile extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+public class MobileInfo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mobile);
+        setContentView(R.layout.activity_mobile_info);
+        setDeviceModelInfo();
+        setEMIInfo();
+        setBuildNumberInfo();
+        setAndroidVersionInfo();
+        setSecurityUpdateInfo();
+        setManufactureNumberInfo();
     }
-}
+
+
+
+    private void setDeviceModelInfo() {
+        TextView deviceModelViewText = findViewById(R.id.deviceModelViewText);
+        deviceModelViewText.setText("Model No: " + Build.MODEL);
+    }
+
+    private void setEMIInfo() {
+        TextView emiTextView = findViewById(R.id.emiTextView);
+        emiTextView.setText("Device Manufacturer: "+ Build.MANUFACTURER);
+    }
+
+    private void setBuildNumberInfo() {
+        TextView buildNumberTextView = findViewById(R.id.buildNumberTextView);
+        buildNumberTextView.setText("Build Number: " + Build.DISPLAY);
+    }
+
+    private void setAndroidVersionInfo() {
+        TextView androidVersionTextView = findViewById(R.id.androidVersionTextView);
+        androidVersionTextView.setText("Android Version: " + Build.VERSION.RELEASE);
+    }
+
+    private void setSecurityUpdateInfo() {
+        TextView securityUpdateTextView = findViewById(R.id.securityUpdateTextView);
+        securityUpdateTextView.setText("Serial Number: "+ Build.TIME);
+    }
+
+    private void setManufactureNumberInfo() {
+        TextView manufactureNumber = findViewById(R.id.manufactureNumber);
+        manufactureNumber.setText("CPU: " + Build.CPU_ABI);
+    }
+
+}`
